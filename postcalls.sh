212dfcc4b2427897b6f79b89593309fb6faf62c1
vileda <vileda@vileda.cc>
2015-11-12 23:41:23 +0100

    sleep

--- postcalls.sh
+++ postcalls.sh
@@ -5,5 +5,5 @@ for i in $(eval echo "{1..$1}"); do
 	  -X POST \
 	  --data "from=492111234567&to=4915791234567&direction=in&event=newCall&callId=1234$i6&user[]=Alice&user[]=Bob"\
 	  http://localhost:8080/api/event/$2;
-  	 # sleep 0.1
+  	  sleep 0.05
 done
